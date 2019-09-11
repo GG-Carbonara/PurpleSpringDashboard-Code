d02f6b02a43baa40b46e4f6ee6d8004e654352bf
GG-Carbonara <07xmatix@gmail.com>
2019-09-11 21:52:53 +0800

    Various Adjustments

--- PurpleSpringDashboard/scripts.js
+++ PurpleSpringDashboard/scripts.js
@@ -2,6 +2,54 @@ function SidebarDropdownFunction() {
     document.getElementById("Dropdown").classList.toggle("show");
   }
 
-function SummaryDisplayFunction() {
-    document.getElementById("Summary").style.display = "block";
-  }
+  function ClientInfoDropdownFunction(){
+    var showDropdown=document.getElementById("ClientInfoDropdown");
+    if (showDropdown.style.display == "none") {
+        showDropdown.style.display = "block";
+    } else {
+        showDropdown.style.display = "none";
+    }       
+}
+
+function CaregiversDataDropdownFunction(){
+    var showDropdown=document.getElementById("CaregiversDataDropdown");
+    if (showDropdown.style.display == "none") {
+        showDropdown.style.display = "block";
+    } else {
+        showDropdown.style.display = "none";
+    }       
+}
+
+function DocumentsDropdownFunction(){
+    var showDropdown=document.getElementById("DocumentsDropdown");
+    if (showDropdown.style.display == "none") {
+        showDropdown.style.display = "block";
+    } else {
+        showDropdown.style.display = "none";
+    }       
+}
+
+
+function FormsDropdownFunction(){
+    var showDropdown=document.getElementById("FormsDropdown");
+    if (showDropdown.style.display == "none") {
+        showDropdown.style.display = "block";
+    } else {
+        showDropdown.style.display = "none";
+    }       
+}
+
+
+
+function SummaryDisplayFunction(){
+    var showDropdown=document.getElementById("Summary");
+    if (showDropdown.style.display == "none") {
+        showDropdown.style.display = "block";
+    } else {
+        showDropdown.style.display = "none";
+    }        
+}
+
+function isActive(){
+    
+}
