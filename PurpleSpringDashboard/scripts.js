b32730fd1d92f8f7dd9be2b6dd28f6f01b25ac71
GG-Carbonara <07xmatix@gmail.com>
2019-09-12 17:54:01 +0800

    Organized Functions

--- PurpleSpringDashboard/scripts.js
+++ PurpleSpringDashboard/scripts.js
@@ -1,55 +1,54 @@
+// var newDate = new Date();
+// newDate.setDate(newDate.getDate() + 1);
+
+// document.getElementById('displayDate').innerHTML = (newDate.getMonth() + 1) + '/' + newDate.getDate() + '/' + newDate.getFullYear();
+function display(id){;
+    if (id.style.display == "none") {
+        id.style.display = "block";
+    } else {
+        id.style.display = "none";
+    }           
+}
+
+
+
 function SidebarDropdownFunction() {
-    document.getElementById("Dropdown").classList.toggle("show");
+    var x = document.getElementById("Dropdown");
+    display(x);
   }
 
+
   function ClientInfoDropdownFunction(){
-    var showDropdown=document.getElementById("ClientInfoDropdown");
-    if (showDropdown.style.display == "none") {
-        showDropdown.style.display = "block";
-    } else {
-        showDropdown.style.display = "none";
-    }       
+    var x = document.getElementById("ClientInfoDropdown");
+    display(x);
 }
 
 function CaregiversDataDropdownFunction(){
-    var showDropdown=document.getElementById("CaregiversDataDropdown");
-    if (showDropdown.style.display == "none") {
-        showDropdown.style.display = "block";
-    } else {
-        showDropdown.style.display = "none";
-    }       
+    var x = document.getElementById("CaregiversDataDropdown");
+    display(x);
 }
 
 function DocumentsDropdownFunction(){
-    var showDropdown=document.getElementById("DocumentsDropdown");
-    if (showDropdown.style.display == "none") {
-        showDropdown.style.display = "block";
-    } else {
-        showDropdown.style.display = "none";
-    }       
+    var x = document.getElementById("DocumentsDropdown");
+    display(x);
 }
 
 
 function FormsDropdownFunction(){
-    var showDropdown=document.getElementById("FormsDropdown");
-    if (showDropdown.style.display == "none") {
-        showDropdown.style.display = "block";
-    } else {
-        showDropdown.style.display = "none";
-    }       
+    var x = document.getElementById("FormsDropdown");
+    display(x);
 }
-
 
 
 function SummaryDisplayFunction(){
-    var showDropdown=document.getElementById("Summary");
-    if (showDropdown.style.display == "none") {
-        showDropdown.style.display = "block";
-    } else {
-        showDropdown.style.display = "none";
-    }        
+    var x = document.getElementById("Summary");
+    display(x);
 }
+SummaryDisplayFunction();
 
-function isActive(){
+
+
+
+// function isActive(){
     
-}
+// }
