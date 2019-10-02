2777d577a14b07b6f68044d0f54d6145b7c48cc7
GG-Carbonara <07xmatix@gmail.com>
2019-10-03 00:51:34 +0800

    began work on login

---
+++ PurpleSpringDashboard/login.php
@@ -0,0 +1,19 @@
+
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Page Title</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='login-style.css'>
+</head>
+<body>
+    <div>
+    <form>
+        <input type="text" name="mailuid" placeholder="Username/E-mail...">
+        <input type="password" name="pwd" placeholder="Password...">
+        <button type="submit" name="login-submit">Login</button>
+    </form>
+    </div>
+</body>
+</html>
